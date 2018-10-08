610e1f838bfaf8db43d88c4b902d2e2929e24f12
Jama Noor <jamawadi.noor@acando.no>
2018-10-08 16:05:58 +0200

    fixed hobbyist not working without devtools

--- src/logic/store.js
+++ src/logic/store.js
@@ -1,20 +1,15 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { reducers } from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-let devTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-if (process.env.NODE_ENV === "prod" || process.env.NODE_ENV === "production") {
-  devTools = a => a;
+// Exportert funksjon som oppretter store
+
+let enhancement;
+if (process.env.NODE_ENV === "production") {
+  enhancement = compose(applyMiddleware(thunk));
+} else {
+  enhancement = composeWithDevTools(applyMiddleware(thunk));
 }
 
-// Exportert funksjon som oppretter store, legg merke til at vi i tilleg bruker
-// https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd
-// dette vil vi ikke gjøre i produksjon, så det bør være logikk som ikke tar med denne med mindre
-// man kjører i dev.
-const enhancer = compose(
-  applyMiddleware(thunk),
-  devTools
-);
-
-export const store = createStore(reducers, enhancer);
+export const store = createStore(reducers, enhancement);
